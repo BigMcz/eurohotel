783adbf3f32b05275615408b8ac438951f87fd6b
unknown <xamax1989@gmail.com>
2022-04-11 22:31:38 +0200

    scroll to top, parallax scrolling, edit css top header

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,42 @@ $(window).on("scroll", function() {
           $('nav').removeClass('black');
     }
 })
+
+//Parallax & fade on scroll	
+		
+function scrollBanner() {
+      $(document).on('scroll', function(){
+          var scrollPos = $(this).scrollTop();
+                if ($(window).width() > 1200) {
+                      $('.parallax-top').css({
+                        'top' : (scrollPos/2.5)+'px'
+                      });
+                      $('.parallax-fade-top').css({
+                        'top' : (scrollPos/2)+'px',
+                        'opacity' : 1-(scrollPos/750)
+                      });
+                      $('.fade-top').css({
+                        'opacity' : 1-(scrollPos/350)
+                      });
+                }	
+      });    
+    }
+    scrollBanner();
+
+    //Scroll back to top
+	
+		var offset = 300;
+		var duration = 400;
+		jQuery(window).on('scroll', function() {
+			if (jQuery(this).scrollTop() > offset) {
+				jQuery('.scroll-to-top').fadeIn(duration);
+			} else {
+				jQuery('.scroll-to-top').fadeOut(duration);
+			}
+		});
+				
+		jQuery('.scroll-to-top').on('click', function(event) {
+			event.preventDefault();
+			jQuery('html, body').animate({scrollTop: 0}, duration);
+			return false;
+		})
--- index.php
+++ index.php
@@ -2,12 +2,67 @@
 
 <section class="home">
     <header class="home_top">
-        
+        <div class="center_title">
+
+            <h1 class="maintitle parallax-fade-top"> Eurohotel Karlovy Vary</h1>
+            <h2 class="stars parallax-fade-top">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </h2>
+        </div>
     </header>
 
     <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
     </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eum earum error exercitationem veniam officia adipisci quaerat vitae nam assumenda laudantium quod esse eius reprehenderit quae, soluta eaque iure quidem?
+    </p>
+    
 
 </section>
 
